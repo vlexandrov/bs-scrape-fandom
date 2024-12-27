76e90bee6413cd01e14f2859a1aaab11a8acd8c5
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2024-12-27 19:06:14 +1300

    add code for processing tables and formatting to html

--- main.py
+++ main.py
@@ -21,3 +21,22 @@ output_html = """
 <body>
 """
 
+# processing
+for index, table in enumerate(tables):
+    output_html += f"<h2>Table {index + 1}</h2>\n"
+    tbody = table.find('tbody')
+    if tbody:
+        # remove icons, stored in divs in some column
+        rows_to_remove = tbody.find_all('tr', recursive=False)
+        for row in rows_to_remove:
+            # if row contains div with icons, remove
+            if row.find('div', class_='center'):
+                row.decompose()
+    output_html += str(table)
+    output_html += "\n"
+
+# close multiline for exporting
+output_html += """
+</body>
+</html>
+"""
