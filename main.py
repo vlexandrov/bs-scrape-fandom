74e0c9c43597a239a5cd945aadc7ab4fff44ddbb
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2024-12-27 18:54:39 +1300

    add code to load html file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from bs4 import BeautifulSoup
 
-# Load html
+# Load html from file path
+file_path = "input/fandom-tama-wiki.html"
+with open(file_path, 'r', encoding='utf-8') as file:
+    html_content = file.read()
